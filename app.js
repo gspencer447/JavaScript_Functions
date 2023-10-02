fdfe4b2732229c5553d0d0ffb80207765c5e4951
Graham Spencer <gspencer447@yahoo.com>
2023-10-02 14:55:49 -0500

    completed exercise 1

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+function printOdds(count) {
+  for (i = 1; i <= 100; i++) {
+    if (i % 2 === 1) {
+      console.log(i);
+    } else {
+      console.log("This number is even");
+    }
+  }
+}
+
+printOdds();
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
