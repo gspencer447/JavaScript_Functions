caa9d8b37f00ef6f84f0d5ee64550b5fcd10840f
Graham Spencer <gspencer447@yahoo.com>
2023-10-03 13:36:08 -0500

    completed all steps of exercises 1-4

--- app.js
+++ app.js
@@ -15,4 +15,50 @@ function printOdds(count) {
 printOdds();
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+function checkAge(userName, age) {
+  let aboveSixteen = `Congrats ${userName}, you can drive!`;
+  let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+  if (age >= 16) {
+    console.log(aboveSixteen);
+  } else {
+    console.log(belowSixteen);
+  }
+}
+
+checkAge("Graham", 14);
+
+//Exercise 3 Section
+function whichQuadrant(x, y) {
+  if (x == 0 && y == 0) {
+    console.log(`The point ${x},${y} is located on the origin`);
+  } else if (x == 0) {
+    console.log(`The point ${x},${y} is located on the Y-axis.`);
+  } else if (y == 0) {
+    console.log(`The point ${x},${y} is located on the X-axis.`);
+  } else if (x > 0 && y > 0) {
+    console.log(`The point ${x},${y} is located in quadrant 1.`);
+  } else if (x < 0 && y > 0) {
+    console.log(`The point ${x},${y} is located in quadrant 2.`);
+  } else if (x < 0 && y < 0) {
+    console.log(`The point ${x},${y} is located in quadrant 3.`);
+  } else {
+    console.log(`The point ${x},${y} is located in quadrant 4.`);
+  }
+}
+
+whichQuadrant(0, 0);
+
+//Exercise 4 Section
+function typeOfTriangle(a, b, c) {
+  if (a + b <= c || b + c <= a || c + a <= b) {
+    console.log("This is an invalid triangle.");
+  } else if (a == b && a == c && b == c) {
+    console.log("This is an equilateral triangle.");
+  } else if (a == b || a == c || b == c) {
+    console.log("This is an isosceles Triangle.");
+  } else if (a != b && a != c && b != c) {
+    console.log("This is a scalene Triangle.");
+  }
+}
+
+typeOfTriangle(3, 6, 8);
